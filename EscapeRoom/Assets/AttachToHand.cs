bd4de69a3aec7f49a30d33c7acfda41be22724b1
Sean <seandon47@gmail.com>
2017-08-25 18:40:56 -0400

    Added Cargo Bay and Engineering Rooms, Added "Watch"

---
+++ EscapeRoom/Assets/AttachToHand.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttachToHand : MonoBehaviour {
+
+    public GameObject rightHand, watch;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        watch.transform.position = rightHand.transform.position - (rightHand.transform.forward * .3f);
+        watch.transform.rotation = new Quaternion(rightHand.transform.rotation.x, rightHand.transform.rotation.y, rightHand.transform.rotation.z, rightHand.transform.rotation.w);
+    }
+}
