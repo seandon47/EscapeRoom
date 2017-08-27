1d24a5b997db591eb52924b8a035b49f619a61cd
Jonathan <jonr79@hotmail.com>
2017-08-27 10:58:33 -0400

    Added Robot Friend
    A little robot friend that the player on the main screen can play.
    Also with a rudimentary ship minimap.

---
+++ EscapeRoom/Assets/MouseRotate.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseRotate : MonoBehaviour {
+
+    private float yaw = 0;
+    private float pitch = 0;
+    public GameObject HeadObject;
+
+	// Use this for initialization
+	void Start () {
+
+        Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.lockState = CursorLockMode.None;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        yaw += 2 * Input.GetAxis("Mouse X");
+        pitch += 2 * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0);
+        
+        HeadObject.transform.eulerAngles = new Vector3(0, yaw, 0);
+    }
+}
---
+++ EscapeRoom/Assets/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public GameObject Director;
+    public GameObject Follower;
+    public float FollowerX;
+    public float FollowerY;
+    public float FollowerZ;
+
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void FixedUpdate()
+    {
+        float xOffset = transform.position.x + FollowerX;
+        float yOffset = transform.position.y + FollowerY;
+        float zOffset = transform.position.z + FollowerZ;
+        Follower.transform.position = new Vector3(xOffset, yOffset, zOffset);
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        movement = Director.transform.rotation * movement;
+
+        rb.velocity = movement * speed;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "PlayerRobot")
+        {
+            Physics.IgnoreCollision(GetComponent<Collider>(), collision.gameObject.GetComponent<Collider>());
+        }
+    }
+}
