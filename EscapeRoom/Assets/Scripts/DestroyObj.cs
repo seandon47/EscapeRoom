2d8f606a1b855469d553458eb8b4f82e93fd01dc
Ben Susla <33492136+bsus37@users.noreply.github.com>
2018-05-17 10:41:08 -0400

    i made a block

---
+++ EscapeRoom/Assets/Scripts/DestroyObj.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObj : MonoBehaviour {
+
+    public GameObject baroquePrefab;
+
+    void OnMouseDown()
+    {
+        BreakThatShit();
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.relativeVelocity.magnitude > 2f)
+        {
+            BreakThatShit();
+        }
+    }
+	
+	// Update is called once per frame
+	void BreakThatShit ()
+    {
+        if (baroquePrefab)
+        {
+            Instantiate(baroquePrefab, transform.position, transform.rotation);
+        }
+        Destroy(gameObject);
+
+	}
+}
