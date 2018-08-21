c15c1468e8d39e1c89bab4823e08d314abd0dcd5
Jonathan <jonr79@hotmail.com>
2018-08-20 21:14:16 -0400

    Added scripts for menu classes

---
+++ EscapeRoom/Assets/Scripts/Door/DoorMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DoorMenu : MonoBehaviour {
+    // Reference to the content of the scrollable window
+    // All of the door panels will be put in there.
+    public Transform Content;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ EscapeRoom/Assets/Scripts/Door/DoorPanelScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DoorPanelScript : MonoBehaviour {
+
+    Door MyDoor;
+    public Text NameLabel;
+    public Text StatusLabel;
+    public Button LockButton;
+    public Button OpenButton;
+
+	// Use this for initialization
+	void Start ()
+    {
+        LockButton.onClick.AddListener(LockButtonClick);
+        OpenButton.onClick.AddListener(OpenButtonClick);
+    }
+
+    public void Setup(string Name, Door InDoor)
+    {
+        MyDoor = InDoor;
+        NameLabel.text = Name;
+        StatusLabel.text = MyDoor.IsBroken ? "Malfunctioning" : "Functioning";
+        LockButton.GetComponentInChildren<Text>().text = MyDoor.IsLocked ? "Locked" : "Unlocked";
+        OpenButton.GetComponentInChildren<Text>().text = MyDoor.IsOpen ? "Open" : "Closed";
+    }
+
+    public void LockButtonClick()
+    {
+        if (MyDoor.IsLocked)
+        {
+            MyDoor.IsLocked = false;
+        }
+        else
+        {
+            MyDoor.IsLocked = true;
+        }
+        LockButton.GetComponent<Text>().text = MyDoor.IsLocked ? "Locked" : "Unlocked";
+    }
+
+    public void OpenButtonClick()
+    {
+        // Run animation coroutine
+        MyDoor.ToggleDoor();
+        if (MyDoor.IsOpen)
+        {
+            MyDoor.IsOpen = false;
+        }
+        else
+        {
+            MyDoor.IsOpen = true;
+        }
+        OpenButton.GetComponent<Text>().text = MyDoor.IsOpen ? "Open" : "Closed";
+    }
+}
