e8264bb36a4c77cfd6a642c3fbbb4660124308ec
Jonathan <jonr79@hotmail.com>
2018-08-26 13:13:50 -0400

    Door names are now displayed on the minimap

--- EscapeRoom/Assets/Scripts/Door/Door.cs
+++ EscapeRoom/Assets/Scripts/Door/Door.cs
@@ -11,10 +11,27 @@ public class Door : MonoBehaviour {
     public bool IsOpen;
     public bool isOpenAtStart = false;
 
+    GameObject MiniMapLabel;
+
 	// Use this for initialization
 	void Start ()
     {
-		
+        StatusIcon.transform.eulerAngles = new Vector3(90, 0, 0);
+
+        MiniMapLabel = new GameObject();
+        MiniMapLabel.transform.position = StatusIcon.transform.position;
+        MiniMapLabel.transform.SetParent(StatusIcon.transform);
+        MiniMapLabel.transform.localRotation = new Quaternion(0, 0, 0, 0);
+        MiniMapLabel.AddComponent<TextMesh>();
+        MiniMapLabel.layer = 13;
+
+        Vector3 pos = new Vector3(0, 0.75f, 0);
+        MiniMapLabel.transform.localPosition = pos;
+
+        TextMesh TM = MiniMapLabel.GetComponent<TextMesh>();
+        TM.alignment = TextAlignment.Center;
+        TM.anchor = TextAnchor.MiddleCenter;        
+        TM.text = gameObject.name;
 	}
 	
 	// Update is called once per frame
