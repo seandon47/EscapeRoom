e5c66b8c5a4697a1a21bedf317dd9a132103fda6
Jonathan <jonr79@hotmail.com>
2019-01-01 13:04:18 -0500

    Added some breaker boxes to the scene
    Also added Breaker Boxes to the project with a prefab and everything!

---
+++ EscapeRoom/Assets/CustomAssets/Breaker/Breaker.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breaker : MonoBehaviour {
+    public BreakerButtonPress TopButton;
+    public BreakerButtonPress BottomButton;
+    Animator anim;
+
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+        if(TopButton != null)
+        {
+            TopButton.Pressed += TopButton_Pressed;
+        }
+
+        if (BottomButton != null)
+        {
+            BottomButton.Pressed += BottomButton_Pressed;
+        }
+    }
+
+    private void TopButton_Pressed()
+    {
+        StartCoroutine(DoRedButtonPress());
+    }
+
+    private void BottomButton_Pressed()
+    {
+        StartCoroutine(DoGreenButtonPress());
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+
+    IEnumerator DoRedButtonPress()
+    {
+        anim.Play("TopButtonPress");
+        yield return new WaitForSeconds(1.0f);
+    }
+
+    IEnumerator DoGreenButtonPress()
+    {
+        anim.Play("BottomButtonPress");
+        yield return new WaitForSeconds(1.0f);
+    }
+}
---
+++ EscapeRoom/Assets/CustomAssets/Breaker/BreakerButtonPress.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakerButtonPress : MonoBehaviour {
+
+    public delegate void ButtonPressedEvent();
+    public event ButtonPressedEvent Pressed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (Pressed != null)
+            Pressed.Invoke();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (Pressed != null)
+            Pressed.Invoke();
+    }
+}
