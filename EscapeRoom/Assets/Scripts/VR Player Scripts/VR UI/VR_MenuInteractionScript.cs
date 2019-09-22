16f98fa64a328255f9c5ec6b7d0557509f7d138d
Jonathan <jonr79@hotmail.com>
2019-09-22 16:03:17 -0400

    Implemented turning the menu off

---
+++ EscapeRoom/Assets/Scripts/VR Player Scripts/VR UI/VR_MenuInteractionScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class VR_MenuInteractionScript : MonoBehaviour
+{
+    public SteamVR_Action_Boolean MenuOnOff;
+    public SteamVR_Input_Sources HandType;
+    public GameObject Menu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MenuOnOff.AddOnChangeListener(ButtonDown, HandType);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ButtonDown(SteamVR_Action_In fromAction)
+    {
+        if (fromAction.GetChanged(HandType))
+        {
+            if(MenuOnOff.GetStateDown(HandType))
+            {
+                Menu.SetActive(!Menu.activeInHierarchy);
+                Debug.Log("Menu Button Click");
+            }
+        }
+    }
+}
