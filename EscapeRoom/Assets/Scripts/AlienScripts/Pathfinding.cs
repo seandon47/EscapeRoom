1f211bc5b179b1f07aebcfbfc5d92d6709ba2fcd
Sean <seandon47@gmail.com>
2020-04-02 19:48:46 -0400

    Alien has extremely rudimentary AI

--- EscapeRoom/Assets/Scripts/AlienScripts/Pathfinding.cs
+++ EscapeRoom/Assets/Scripts/AlienScripts/Pathfinding.cs
@@ -7,17 +7,38 @@ public class Pathfinding : MonoBehaviour
 {
 
     public Transform target;
+    public Transform[] points;
+    private int destPoint = 0;
     NavMeshAgent agent;
 
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+
+        GotoNextPoint();
+    }
+
+    void GotoNextPoint()
+    {
+        // Returns if no points have been set up
+        if (points.Length == 0)
+            return;
+
+        // Set the agent to go to the currently selected destination.
+        agent.destination = points[destPoint].position;
+
+        // Choose the next point in the array as the destination,
+        // cycling to the start if necessary.
+        destPoint = (destPoint + 1) % points.Length;
     }
 
     // Update is called once per frame
     void Update()
     {
-        agent.SetDestination(target.position);
+        if (!agent.pathPending && agent.remainingDistance < 5.5f)
+            GotoNextPoint();
+
+        //agent.SetDestination(target.position);
     }
 }
