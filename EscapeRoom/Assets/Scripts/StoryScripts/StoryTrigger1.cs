aedca32d69de64dce7caae91e70c6884aca20502
Sean <seandon47@gmail.com>
2020-06-09 18:45:36 -0400

    New Voicelines For Story Guidance
    Triggers and voiceliness

---
+++ EscapeRoom/Assets/Scripts/StoryScripts/StoryTrigger1.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryTrigger1 : MonoBehaviour
+{
+    public AudioClip audioClip;
+    AudioSource audio;
+    public GameObject storyTrigger;
+    private bool isTriggered = false; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = storyTrigger.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if(!isTriggered)
+        {
+            if (collision.gameObject.name == "Drone Body")
+            {
+                audio.PlayOneShot(audioClip);
+            }
+            isTriggered = true;
+        }
+    }
+}
