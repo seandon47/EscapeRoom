1eb882ce19e0b5744412e5a5c97a89702c6bf467
Sean <seandon47@gmail.com>
2020-11-22 14:10:07 -0500

    Alien Can Now Kill Player
    Kills on collision with hands or body

---
+++ EscapeRoom/Assets/AlienScripting.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlienScripting : MonoBehaviour
+{
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "HandColliderLeft(Clone)" || collision.gameObject.name == "HandColliderRight(Clone)" collision.gameObject.name == "BodyCollider")
+        {
+            Application.Quit();
+        }
+
+    }
+}
