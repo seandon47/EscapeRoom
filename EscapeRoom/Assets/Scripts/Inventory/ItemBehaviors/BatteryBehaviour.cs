3fd177049806bc3109489c0ab71a285c304dd835
Jonathan <jonr79@hotmail.com>
2021-02-03 22:08:04 -0500

    add battery behavior make battery throwable and mountable

---
+++ EscapeRoom/Assets/Scripts/Inventory/ItemBehaviors/BatteryBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatteryBehaviour : ItemBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public override GameObject GetVrBehaviorComponent()
+    {
+        // Show battery percentage on watch
+        throw new System.NotImplementedException();
+    }
+
+    public override void Input()
+    {
+        // Come up with something fun to do here
+        throw new System.NotImplementedException();
+    }
+}
