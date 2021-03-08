7bbaedc31b5d05a4d2caa75ab9e742bd289eac7d
Jonathan <jonr79@hotmail.com>
2021-03-07 21:33:08 -0500

    Initial add of BatteryOverlay for drone

---
+++ EscapeRoom/Assets/BatteryOverlay.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatteryOverlay : MonoBehaviour
+{
+    public Fadable BlackoutImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    internal void PowerDown()
+    {
+        BlackoutImage.Show();
+    }
+
+    public void PowerUp()
+    {
+        BlackoutImage.Fade();
+    }
+}
