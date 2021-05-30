d457fdb501420d08c9463a417e144913ec21441e
Jonathan <jonr79@hotmail.com>
2021-05-30 15:44:49 -0400

    Initial implementation of battery behaviour
    Update the text of the watch display to have the battery percent on it.

--- EscapeRoom/Assets/Scripts/Inventory/ItemBehaviors/BatteryBehaviour.cs
+++ EscapeRoom/Assets/Scripts/Inventory/ItemBehaviors/BatteryBehaviour.cs
@@ -1,24 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class BatteryBehaviour : ItemBehaviour
 {
+    public WatchDisplay BatteryReadout;
+    public UnityEvent BatteryUpdate;
+
+    BatteryClass Battery;
+    WatchDisplay Display;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Battery = GetComponent<BatteryClass>();
+        if (Battery == null)
+            Battery = new GameObject().AddComponent<RemovedBatteryClass>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Display == null)
+            return;
+
+        Display.UpdateDisplayText($"{Battery.GetBatteryPercent()}%");
     }
     public override void EquipToVrPlayer(Watch watch)
     {
         // Show battery percentage on watch
-        throw new System.NotImplementedException();
+        Display = Instantiate(BatteryReadout);
+        watch.AddButton(Display.gameObject);
     }
 
     public override void Input()
