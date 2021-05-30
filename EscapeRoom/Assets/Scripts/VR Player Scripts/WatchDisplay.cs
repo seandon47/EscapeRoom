72ec48e36e54fc6e775e34418c578afeaa5a51b6
Jonathan <jonr79@hotmail.com>
2021-05-30 15:44:03 -0400

    Initial commit of WatchDisplay prefab and script

---
+++ EscapeRoom/Assets/Scripts/VR Player Scripts/WatchDisplay.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class WatchDisplay : MonoBehaviour
+{
+    public TextMeshPro DisplayText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateDisplayText(string text)
+    {
+        DisplayText.text = text;
+    }
+}
